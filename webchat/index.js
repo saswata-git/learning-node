fa0d3ce434f325aa2d326a0af4c62bb70ba63887
saswatadev <saswata.freelancer@gmail.com>
2016-12-16 22:40:07 +0530

    bootstrap added

--- webchat/index.js
+++ webchat/index.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 var port = 3700;
 
-app.set('views', __dirname+'/tpl');
+app.set('views', __dirname+'/template');
 app.set('view engine', "ejs");
 app.engine('html', require('ejs').renderFile);
 
-app.get("/", function(req, res){
-    res.render("page.html");
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
 });
 
-app.use(express.static(__dirname+'/public'));
+router.get("/",function(req,res){
+  res.render("index.html");
+});
+
+router.get("/about",function(req,res){
+  res.render("about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.render("contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.render("404.html");
+});
+
+
+
+
+// app.get("/", function(req, res){
+//     res.render("page.html");
+// });
+
+app.use(express.static(__dirname+'/assets'));
 
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
