4f84dc6b511f12df80f6057693383a8486a8f608
Saswata Pal <saswata.pal@massoftind.com>
2016-12-17 17:31:11 +0530

    mean stack added

---
+++ webchat/app/routes.js
@@ -0,0 +1,94 @@
+module.exports = function(app, router, User) {
+	
+	router.use(function (req,res,next) {
+	  console.log("/" + req.method);
+	  next();
+	});
+
+	router.get("/",function(req,res){
+	  res.render("index.html");
+	});
+
+	router.get("/about",function(req,res){
+	  res.render("about.html");
+	});
+
+	router.get("/contact",function(req,res){
+	  res.render("contact.html");
+	});
+
+	//add user
+	router.route('/api/users')
+	  // create a bear (accessed at POST http://localhost:8080/api/bears)
+	  .post(function(req, res) {
+	      var user = new User();      // create a new instance of the User model
+	      user.firstName = req.body.firstName;  // set the user name (comes from the request)
+	      user.lastName = req.body.lastName; 
+	      // save the bear and check for errors
+	      user.save(function(err) {
+	          if (err)
+	              res.send(err);
+
+	          res.json({ message: 'User created!' });
+	      });
+	      
+	  })
+
+	//fetch all users 
+	.get(function(req, res) {
+	  User.find(function(err, users){
+	    if(err)
+	      res.send(err);
+
+	    res.json(users);
+	  })
+	})
+
+	router.route('/api/users/:userId')
+
+	.get(function(req, res){
+	  User.findById(req.params.userId, function(err, user){
+	    if(err)
+	      res.send(err);
+
+	    res.json(user);
+
+	  })
+	})
+
+	.put(function(req, res){
+	  User.findById(req.params.userId, function(err, user){
+	    if(err)
+	      res.send(err);
+
+	    user.firstName = req.body.firstName;  // set the user name (comes from the request)
+	    user.lastName = req.body.lastName; 
+	    // save the bear and check for errors
+	    user.save(function(err) {
+	        if (err)
+	            res.send(err);
+
+	        res.json({ message: 'User updated!' });
+	    });
+
+	  })
+	})
+
+	.delete(function(req, res){
+	  User.remove({
+	    _id: req.params.userId
+	  }, function(err, user){
+	    if(err)
+	      res.send(err);
+
+	    res.json({ message: 'User successfully deleted!' });
+
+	  })
+	})
+
+	app.use("/",router);
+
+	app.use("*",function(req,res){
+	  res.render("404.html");
+	});
+}
--- webchat/index.js
+++ webchat/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var mongoose   = require('mongoose');
 var port = process.env.PORT || 3700;
-
 var bodyParser = require('body-parser');
+
+// config files
+var db = require('./config/db');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,102 +18,10 @@ app.set('view engine', "ejs");
 app.engine('html', require('ejs').renderFile);
 
 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
-});
+var User     = require('./app/models/user');
 
-router.get("/",function(req,res){
-  res.render("index.html");
-});
-
-router.get("/about",function(req,res){
-  res.render("about.html");
-});
-
-router.get("/contact",function(req,res){
-  res.render("contact.html");
-});
-
-//add user
-router.route('/api/users')
-  // create a bear (accessed at POST http://localhost:8080/api/bears)
-  .post(function(req, res) {
-      var user = new User();      // create a new instance of the User model
-      user.firstName = req.body.firstName;  // set the user name (comes from the request)
-      user.lastName = req.body.lastName; 
-      // save the bear and check for errors
-      user.save(function(err) {
-          if (err)
-              res.send(err);
-
-          res.json({ message: 'User created!' });
-      });
-      
-  })
-
-//fetch all users 
-.get(function(req, res) {
-  User.find(function(err, users){
-    if(err)
-      res.send(err);
-
-    res.json(users);
-  })
-})
-
-router.route('/api/users/:userId')
-
-.get(function(req, res){
-  User.findById(req.params.userId, function(err, user){
-    if(err)
-      res.send(err);
-
-    res.json(user);
-
-  })
-})
-
-.put(function(req, res){
-  User.findById(req.params.userId, function(err, user){
-    if(err)
-      res.send(err);
-
-    user.firstName = req.body.firstName;  // set the user name (comes from the request)
-    user.lastName = req.body.lastName; 
-    // save the bear and check for errors
-    user.save(function(err) {
-        if (err)
-            res.send(err);
-
-        res.json({ message: 'User updated!' });
-    });
-
-  })
-})
-
-.delete(function(req, res){
-  User.remove({
-    _id: req.params.userId
-  }, function(err, user){
-    if(err)
-      res.send(err);
-
-    res.json({ message: 'User successfully deleted!' });
-
-  })
-})
-
-
-app.use("/",router);
-
-app.use("*",function(req,res){
-  res.render("404.html");
-});
-
-// app.get("/", function(req, res){
-//     res.render("page.html");
-// });
+// routes ==================================================
+require('./app/routes')(app, router, User); // pass our application into our routes
 
 app.use(express.static(__dirname+'/assets'));
 
@@ -126,7 +37,5 @@ io.sockets.on('connection', function (socket) {
 });
 
 //database connection
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost/express'); 
-
-var User     = require('./app/models/user');
+mongoose.connect(db.url); 
+exports = module.exports = app;             // expose app
