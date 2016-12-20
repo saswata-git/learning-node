50c6716acf289854a412157be86b866003d8a26f
Saswata Pal <saswata.pal@massoftind.com>
2016-12-20 19:01:35 +0530

    bootstrap learninf added

--- loopback/server/boot/root.js
+++ loopback/server/boot/root.js
@@ -1,10 +1,8 @@
-module.exports = function(app) {
+'use strict';
+
+module.exports = function(server) {
   // Install a `/` route that returns server status
   // var router = server.loopback.Router();
   // router.get('/', server.loopback.status());
   // server.use(router);
-
-  app.get('/ping', function(req, res) {
-    res.send('pong');
-  });
 };
